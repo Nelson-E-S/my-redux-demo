65d8025ded49481bf53feee484d2497a20fceeb5
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-11 17:15:54 -0400

    installed and tested axios implementation

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -1,3 +1,6 @@
+//justing testing with axios
+import axios from 'axios';
+
 //Create Redux action types
 export const GET_POSTS = 'GET_POSTS';
 export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
@@ -19,8 +22,9 @@ export function fetchPosts(){
         dispatch(getPosts())
 
         try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data = await response.json();
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+                                        .then(res=>res.data)//fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await response;//.json();  //if using axios you won't need to call the json method as long as you specifically grab the data from the axios response like in the previous line
 
             dispatch(getPostsSuccess(data));
         } catch (error){
