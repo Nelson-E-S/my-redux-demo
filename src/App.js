9f98b2b877c6bc1a8a1832955c55a9f5c8ffd7ca
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-11 16:56:16 -0400

    finished implementing api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+import DashboardPage from './pages/DashboardPage';
+import PostsPages from './pages/PostsPages';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        hello, Redux
+        <Router>
+          <Switch>
+            <Route exact path="/" component={DashboardPage} />
+            <Route exact path="/posts" component={PostsPages} />
+            <Redirect to="/" />
+          </Switch>
+        </Router>
       </header>
     </div>
   );
